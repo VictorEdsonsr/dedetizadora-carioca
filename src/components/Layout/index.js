5e9382462e6212c3616a139e54a6fd3e22d170b9
Victor <edvictorsr@gmail.com>
2023-02-04 18:04:42 -0400

    fix: bug in slidr

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -25,7 +25,7 @@ export const Layout = ({ children }) => {
         const scrollTop = window.scrollY;
         const fixedClass =
             scrollTop >= 1
-                ? "fixed top-0 left-0 right-0 shadow shadow-xl transition-all bg-blue-600 max-h-24 z-50 flex justify-between p-7 items-center"
+                ? "fixed top-0 left-0 right-0 shadow shadow-xl transition-all bg-gray-300 max-h-24 z-50 flex justify-between p-7 items-center"
                 : "";
         setFixed(fixedClass);
     };
@@ -36,7 +36,7 @@ export const Layout = ({ children }) => {
                 className={
                     fixed
                         ? fixed
-                        : "flex justify-between p-7 w-full bg-blue-600 items-center max-h-24 shadow shadow-xl"
+                        : "flex justify-between p-7 w-full bg-gray-300 items-center max-h-24 transition-all shadow-xl"
                 }
             >
                 <Link href="/">
@@ -49,7 +49,7 @@ export const Layout = ({ children }) => {
 
             <main>{children}</main>
 
-            <footer className="bg-blue-600 flex flex-col items-center justify-center">
+            <footer className="bg-gray-300 flex flex-col items-center justify-center">
                 <div className="container flex flex-col sm:flex-row items-center justify-between text-white p-3">
 
 
--- src/components/Slider/index.js
+++ src/components/Slider/index.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
-import principalOne from "../../../public/img/dedetizador-img.jpg";
-import principalTwo from "../../../public/img/casa-img.jpg";
+import principalOne from "../../../public/img/slider-1.jpg";
+import principalTwo from "../../../public/img/casa.jpg";
 
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
@@ -20,7 +20,7 @@ export default function Slider() {
                 spaceBetween={30}
                 centeredSlides={true}
                 autoplay={{
-                    delay: 4500,
+                    delay: 10000,
                     disableOnInteraction: false,
                 }}
                 pagination={{
@@ -30,13 +30,13 @@ export default function Slider() {
                 modules={[Autoplay, Pagination, Navigation]}
                 className="mySwiper"
             >
-                <SwiperSlide className="relative flex flex-col items-center justify-center">
+                <SwiperSlide className="flex flex-col items-center justify-center">
                     <Image
-                        className=" w-full h-[400px] sm:h-[590px]"
+                        className="w-full h-full"
                         src={principalOne}
                         alt="image slide 1"
                     />
-                    <article className="absolute z-10 m-auto text-white  text-center max-w-6xl">
+                    <article className="absolute z-30  m-auto text-white  text-center max-w-6xl">
                         <h1 className="font-extrabold text-md sm:text-5xl my-3">
                             Um acampamento de férias no Interior do Rio de Janeiro 90
                             quilômetros do Aeroporto Santos Dumont
@@ -48,11 +48,11 @@ export default function Slider() {
                         </p>
                     </article>
 
-                    <div className="bg-gradient-to-t from-slate-900 to-transparent absolute top-0 right-0 left-0 bottom-0"></div>
+                    <div className="bg-gradient-to-tl from-black to-transparent absolute top-0 right-0 left-0 bottom-0"></div>
                 </SwiperSlide>
                 <SwiperSlide>
                     <Image
-                        className=" w-full h-[400px] sm:h-[590px]"
+                        className=" w-full"
                         src={principalTwo}
                         alt="image slide 2"
                     />
