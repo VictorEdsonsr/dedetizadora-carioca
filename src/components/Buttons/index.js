fd24a7e8abd5f0ff4abc0775d6823dd3e3448939
Victor <edvictorsr@gmail.com>
2023-02-11 14:59:30 -0400

    refactor: modified link of whatsapp button

--- src/components/Buttons/index.js
+++ src/components/Buttons/index.js
@@ -4,7 +4,7 @@ import { FaWhatsapp } from "react-icons/fa";
 export const WhatsappButton = () => {
     return (
         <Link
-            href="https://wa.me/5521970937723"
+            href="https://wa.me/5592984110076"
             className="bg-green-600 fixed z-50 bottom-10 right-10 font-semibold transition-all hover:bg-green-800 shadow-lg p-3 text-center rounded-full"
         >
             <FaWhatsapp color="white" size={40} />
